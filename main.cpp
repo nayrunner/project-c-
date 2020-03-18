2856f4c7491875b91a3a83f16ee959e9c2b64409
charttrakarn0784 <charttrakarn6@hotmail.co.th>
2020-03-18 17:12:10 +0700

    main?

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string command;
+    cout << "======\tTHAI CHESS\t======\n";
+    cout << "[P] Play [H] HowToPlay [E] Exit\n";
+    while(true){
+        cout << "type your command: ";
+        cin >> command;
+        //if ('P')
+        //else if('H')
+        //else if('E')
+        //else    
+        
+    }
+}
+
