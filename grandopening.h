0351028da27a329a59fdb08bfdbed1e667540f3a
nayrunner <Nutchanon_thonganun@cmu.co.th>
2020-03-19 22:46:55 +0700

    opening?

---
+++ grandopening.h
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void grandopening(){
+    cout<<"\n\nTTTTTTTT   HH    HH      AA      IIIIIIII\n";
+    cout<<"   TT      HH    HH    AA  AA    IIIIIIII\n";
+    cout<<"   TT      HHHHHHHH   A      A      II   \n";
+    cout<<"   TT      HHHHHHHH   AAAAAAAA      II\n";
+    cout<<"   TT      HH    HH   AAAAAAAA      II\n";
+    cout<<"   TT      HH    HH   A      A   IIIIIIII\n";
+    cout<<"   TT      HH    HH   A      A   IIIIIIII\n\n";
+    cout<<"CCCCCCCC   HH    HH   EEEEEEEE   SSSSSSSS   SSSSSSSS\n";
+    cout<<"CCCCCCCC   HH    HH   EE         SS         SS      \n";
+    cout<<"CC         HHHHHHHH   EE         SS         SS      \n";
+    cout<<"CC         HHHHHHHH   EEEEEEEE   SSSSSSSS   SSSSSSSS\n";
+    cout<<"CC         HH    HH   EE               SS         SS\n";
+    cout<<"CCCCCCCC   HH    HH   EE               SS         SS\n";
+    cout<<"CCCCCCCC   HH    HH   EEEEEEEE   SSSSSSSS   SSSSSSSS\n\n";
+    cout<<"====================================================\n\n";
+    cout<<"        [P] Play [H] HowToPlay [E] Exit\n\n";
+    cout<<"====================================================\n\n";
+
+
+}
