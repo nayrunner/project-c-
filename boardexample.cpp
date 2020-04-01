706c589350d6b4e689c425a508425b06474bc9b6
charttrakarn0784 <charttrakarn6@hotmail.co.th>
2020-04-01 12:13:55 +0700

    test2.0

--- boardexample.cpp
+++ boardexample.cpp
@@ -1,28 +1,10 @@
 #include<iostream>
 using namespace std;
 
-void makeLine(){
-		cout << "   ==========================================\n";
-	}
-
-void insertPiece(int B[][8]){ 	//! i is row , j is columb
-	int count = 8;
-	for(int i = 0; i < 8 ; i++){
-		cout << count;
-		for(int j = 0;j < 8; j++){
-			cout << " || " << B[i][j] << " ";
-		}
-		count--;
-		cout << "\n";
-		makeLine();
-	}
-}
-
-void makeBoard(int B[][8]){
-	cout << "      A    B    C    D    E    F    G    H   \n";
-	makeLine();
-	insertPiece(B);
-}
+void makeLine();
+void insertPiece(int [][8]);
+void makeBoard(int [][8]);
+char NtoP(int);
 
 int main(){
 	int board[8][8] = {
@@ -58,4 +40,51 @@ int main(){
 	cout << "   ==========================================\n";
 */
 	
+}
+
+/* function */ 
+void makeLine(){
+		cout << "   =========================================\n";
+	}
+
+void insertPiece(int B[][8]){ 	//! i is row , j is columb
+	int count = 8;
+	for(int i = 0; i < 8 ; i++){
+		cout << count;
+		for(int j = 0;j < 8; j++){
+			cout << " || " << NtoP(B[i][j]);
+		}
+		cout << " ||";
+		count--;
+		cout << "\n";
+		makeLine();
+	}
+}
+
+void makeBoard(int B[][8]){
+	cout << "     A    B    C    D    E    F    G    H   \n";
+	makeLine();
+	insertPiece(B);
+}
+
+char NtoP(int N) {  //TODO NtoP is Number to Pieces
+	switch(N){
+		/*Player 2*/
+		case -1: return 'r'; break; 
+		case -2: return 'n'; break;
+		case -3: return 'b'; break;
+		case -4: return 'q'; break;
+		case -5: return 'k'; break;
+		case -6: return 'p'; break;
+		case -7: return 'f'; break;
+		/*Player 1*/
+		case 1: return 'R'; break;
+		case 2: return 'N'; break;
+		case 3: return 'B'; break;
+		case 4: return 'Q'; break; 
+		case 5: return 'K'; break;
+		case 6: return 'P'; break;
+		case 7: return 'F'; break;
+		default: return ' '; break;
+	}
 }
