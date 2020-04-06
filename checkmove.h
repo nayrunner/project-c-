b50acac97dd3afe21d522729777f1d79b6835865
charttrakarn0784 <charttrakarn6@hotmail.co.th>
2020-04-06 20:54:11 +0700

    ver2.1

--- checkmove.h
+++ checkmove.h
@@ -6,7 +6,7 @@ bool Player::checkmove(string move,string com,int pieces){
     if(move == com){return false;}
     // move(0) = แถว , move(1)= บรรทัด
 
-	//rook
+	//*Rook R (เรือ)
 	if(pieces == 1){
 		if(move[0]== com[0]){return true;}
 		else{if(move[1] == com[1]){return true;}
@@ -16,7 +16,7 @@ bool Player::checkmove(string move,string com,int pieces){
 		else{if(move[1] == com[1]){return true;}
 		     else {return false;}	}	}
 	
-	//knight
+	//*Knight N (ม้า) (ที่ไม่เอาไว้ดูด)
 	if (pieces == 2){
 		if( move[0]== (com[0]-1) && move[1] == (com[1]-2) ){return true;}
 		else{if( move[0]== (com[0]-1) && move[1] == (com[1]+2) ){return true;}
@@ -40,7 +40,7 @@ bool Player::checkmove(string move,string com,int pieces){
 									       else{return false;}}}}}}}}
 	}
     
-    //bishop
+    //*Bishop B (โคน)
     if(pieces == 3){
         if( move[1] == (com[1]-1) && move[0] <= (com[0]+1) && move[0] >= (com[0]-1) ){return true;}
         else{
@@ -56,7 +56,7 @@ bool Player::checkmove(string move,string com,int pieces){
         }
     }
 
-    //queen
+    //*Queen Q (เม็ด)
     if(pieces == 4){
         if( move[0] == com[0]+1 || move[0] == com[0]-1 ){
             if( move[1] == com[1]+1 || move[1] == com[1]-1){return true;}
@@ -70,7 +70,7 @@ bool Player::checkmove(string move,string com,int pieces){
         }else{return false;}
     }
 
-    //king
+    //*King K (ขุน)
     if(pieces == 5){
         if( move[0] == com[0] || move[0] == com[0]+1 || move[0] == com[0]-1){
             if( move[1] == com[1] || move[1] == com[1]+1 || move[1] == com[1]-1){return true;}
@@ -84,7 +84,7 @@ bool Player::checkmove(string move,string com,int pieces){
         }else{return false;}
     }
 
-    //pawn
+    //*Pawn P (เบี้ย)
     if(pieces == 6){
         if(move[0] == com[0]){
             if(move[1] == com[1]-1) return true;
@@ -95,6 +95,20 @@ bool Player::checkmove(string move,string com,int pieces){
         if(move[0] == com[0]){
             if(move[1] == com[1]+1) return true;
             else return false;
+        }else{return false;}
+    }
+
+    //* Flipped F (เบี้ยหงาย)
+    if(pieces == 7){
+        if( move[0] == com[0]+1 || move[0] == com[0]-1 ){
+            if( move[1] == com[1]+1 || move[1] == com[1]-1){return true;}
+            else{return false;}
+        }else{return false;}
+    }
+    if(pieces == -7){
+        if( move[0] == com[0]+1 || move[0] == com[0]-1 ){
+            if( move[1] == com[1]+1 || move[1] == com[1]-1){return true;}
+            else{return false;}
         }else{return false;}
     }
 }
